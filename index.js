8722c43be934ae55e35b4b7ce18a874ac4826d16
Manon Pilaud <mandyypilaud@gmail.com>
2019-01-13 11:54:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //Your code here
+function justInvoke(cb){
+    return cb();
+}
+
+function setThisWithCall (cb,thisValue,name){
+  return cb.call(thisValue,name)
+}
+
+function setThisWithApply(cb, thisValue, args){
+  return cb.apply(thisValue,args)
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue){
+  const newFunction = functionToBeCopied.bind(thisValue)
+  return newFunction
+}
